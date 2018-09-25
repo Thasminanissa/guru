449154eaf03dac63d01819a1abc165d7189e31fb
Thasminanissa <42801326+Thasminanissa@users.noreply.github.com>
2018-09-25 15:19:13 +0530

    Update num.py

--- num.py
+++ num.py
@@ -1,9 +1,9 @@
 year = int(input("Enter Year: "))
 if year % 4 == 0 and year % 100 != 0:
-    print(year, "is a Leap Year")
+    print(year, "yes")
 elif year % 100 == 0:
-    print(year, "is not a Leap Year")
+    print(year, "no")
 elif year % 400 ==0:
-    print(year, "is a Leap Year")
+    print(year, "yes")
 else:
-    print(year, "is not a Leap Year")
+    print(year, "no")
